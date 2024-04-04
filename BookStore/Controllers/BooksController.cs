6220face30f8b4962e58084947b08903114b97db
Anna Sonchak <kisukeurahara1328@gmail.com>
2024-04-04 16:54:41 +0200

    Book API upgade, added 'name' and 'genre' query parameters.

--- BookStore/Controllers/BooksController.cs
+++ BookStore/Controllers/BooksController.cs
@@ -1,10 +1,11 @@
 using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using BookStore.Models.ViewModels;
 
 namespace BookStore.Controllers;
 
-[Route ("api/books")]
+[Route ("api/[controller]")]
 [ApiController]
 public class BooksController : Controller
 {
@@ -16,43 +17,66 @@ public class BooksController : Controller
 
     // GET: api/books
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Book>>> GetBooks(bool extended = false)
+    public async Task<ActionResult<IEnumerable<Book>>> GetBooks(
+        [FromQuery] string? name,
+        [FromQuery] string? genre)
     {
-        var query = _bkstrContext.Books;
+        IQueryable<Book> query = _bkstrContext.Books
+            .Include(b => b.BookAuthors)
+                .ThenInclude(ba => ba.IdAuthorNavigation)
+            .Include(b => b.BookGenres)
+                .ThenInclude(bg => bg.IdGenreNavigation);
 
-        if (extended)
-        {
-            var booksWithAuthors = await query
-                .Include(b => b.BookAuthors)
-                    .ThenInclude(ba => ba.IdAuthorNavigation)
-                .Include(b => b.BookGenres)
-                    .ThenInclude(bg => bg.IdGenreNavigation)
-                .Select(b => new
-                {
-                    b.Id,
-                    b.Name,
-                    b.YearOfPublish,
-                    b.Price,
-                    b.CountAvailable,
-                    b.Description,
-                    b.PublishingHouse,
-                    b.Illustrations,
-                    b.Series,
-                    b.Isbn,
-                    b.Language,
-                    b.Translator,
-                    b.OriginalName,
-                    b.Pages,
-                    b.Picture,
-                    AuthorName = string.Join(", ", b.BookAuthors.Select(ba => $"{ba.IdAuthorNavigation.FirstName} {ba.IdAuthorNavigation.LastName}")),
-                    Genres = string.Join(", ", b.BookGenres.Select(bg => $"{bg.IdGenreNavigation.Name}"))
-                })
-                .ToListAsync();
+        if (!string.IsNullOrEmpty(name))
+            query = query.Where(b => b.Name.StartsWith(name));
+        if (!string.IsNullOrEmpty(genre))
+            query = query.Where(b => b.BookGenres.Any(bg => bg.IdGenreNavigation.Name == genre));
 
-            return Ok(booksWithAuthors);
-        }
+        var books = await query.Select(b => MapBook(b)).ToListAsync();
 
-        var books = await query.ToListAsync();
+        if (books.Count == 0)
+            return NoContent();
+
         return Ok(books);
+    }
+
+    // GET: api/books/{id}
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Book>> GetBooks(int id)
+    {
+        var books = await _bkstrContext.Books
+        .Include(b => b.BookAuthors)
+            .ThenInclude(ba => ba.IdAuthorNavigation)
+        .Include(b => b.BookGenres)
+            .ThenInclude(bg => bg.IdGenreNavigation)
+        .Select(b => MapBook(b))
+        .ToListAsync();
+
+        var book = books.FirstOrDefault(b => b.Id == id);
+        return Ok(book);
+    }
+
+    private static BookViewModel MapBook(Book b)
+    {
+        return new BookViewModel
+        {
+            Id = b.Id,
+            Name = b.Name,
+            YearOfPublish = b.YearOfPublish,
+            Price = b.Price,
+            CountAvailable = b.CountAvailable,
+            Description = b.Description,
+            PublishingHouse = b.PublishingHouse,
+            Illustrations = b.Illustrations,
+            Series = b.Series,
+            Isbn = b.Isbn,
+            Language = b.Language,
+            Translator = b.Translator,
+            OriginalName = b.OriginalName,
+            Pages = b.Pages,
+            Picture = b.Picture,
+            Authors = b.BookAuthors.Select(ba => $"{ba.IdAuthorNavigation.FirstName} {ba.IdAuthorNavigation.LastName}"),
+            Genres = b.BookGenres.Select(bg => $"{bg.IdGenreNavigation.Name}")
+        };
     }
 }
